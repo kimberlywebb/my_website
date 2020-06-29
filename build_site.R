732a02ca89c5a2891cd0dac549e8ef3bc9d9f271
Kim Hochstedler <kimhochstedler@gmail.com>
2020-06-29 19:17:13 -0400

    First R Project commit.

---
+++ build_site.R
@@ -0,0 +1,10 @@
+#Set working directory. 
+#This helps avoid confusion if our working directory is 
+#not our site because of other projects we were 
+#working on at the time. 
+setwd("/Users/hochsted/Documents/personal_site")
+
+#Render your site. 
+rmarkdown::render_site()
+
+
