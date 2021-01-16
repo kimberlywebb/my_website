7cd7a23184b8d3da0939cd1b1ce7ddb0bc1b52e7
Kim Hochstedler <kimhochstedler@gmail.com>
2021-01-16 12:26:53 -0500

    Fixing build_site repeat code

--- build_site.R
+++ build_site.R
@@ -2,11 +2,8 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-<<<<<<< HEAD
 setwd("/Users/hochsted/OneDrive/Documents/personal_site/my_website")
-=======
-setwd("/Users/hochsted/Documents/personal_site/my_website")
->>>>>>> 4f3f1c184c1abfae58c413c6d0ac4c071ec0bc07
+
 
 #Render your site. 
 rmarkdown::render_site()
