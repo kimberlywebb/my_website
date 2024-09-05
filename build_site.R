2a5ead17b08f9dd8225e26042740d48d58652d39
Kim Hochstedler Webb <52170271+kimberlywebb@users.noreply.github.com>
2024-09-05 15:17:36 -0400

    update role and themes

--- build_site.R
+++ build_site.R
@@ -1,11 +1,2 @@
-#Set working directory. 
-#This helps avoid confusion if our working directory is 
-#not our site because of other projects we were 
-#working on at the time. 
-setwd("/Users/hochsted/OneDrive/Documents/personal_site/my_website")
-
-
 #Render your site. 
 rmarkdown::render_site()
-
-
